a3ee6d0a52dbfb85ac7324103d91d4c1e5ce62a1
Arttu Tarkiainen <t7taar00@students.oamk.fi>
2019-03-02 16:25:17 +0200

    Create dummy keyPressEvent function

--- QRayEngine/src/userinput.cpp
+++ QRayEngine/src/userinput.cpp
@@ -4,3 +4,21 @@ UserInput::UserInput()
 {
 
 }
+
+void UserInput::keyPressEvent(QKeyEvent *event)
+{
+    event->accept();
+
+    if(event->key() == Qt::Key_A)
+        // do something
+    if(event->key() == Qt::Key_D)
+        // do something
+    if(event->key() == Qt::Key_W)
+        // do something
+    if(event->key() == Qt::Key_S)
+        // do something
+    if(event->key() == Qt::Key_Q)
+        // do something
+    if(event->key() == Qt::Key_E)
+        // do something
+}
--- QRayEngine/src/userinput.h
+++ QRayEngine/src/userinput.h
@@ -1,10 +1,13 @@
 #ifndef USERINPUT_H
 #define USERINPUT_H
 
+#include <QKeyEvent>
+
 class UserInput
 {
 public:
     UserInput();
+    void keyPressEvent(QKeyEvent *event);
 };
 
 #endif // USERINPUT_H
